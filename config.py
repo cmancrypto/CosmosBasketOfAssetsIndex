642c66a6e52bbed725b3b13b895263fba9f8a462
Chris <cmancrypto@outlook.com>
2024-01-13 05:57:06 +1100

    initial config.py
    updated config.py to import coingecko. created tools.py to put utilities

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+##need to configure coingecko_api_key in .env with a demo public API key
+coingecko_api_key = os.getenv("coingecko_api_key")
+
+coingecko_endpoint =f"https://api.coingecko.com/api/v3/coins/"
+##history endpoint syntax is {coin_name}/history
+history_endpoint = f"/history"
+
+
+print(coingecko_api_key)
---
+++ tools.py
@@ -0,0 +1,3 @@
+from config import coingecko_endpoint
+
+
