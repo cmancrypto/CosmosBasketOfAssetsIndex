81c179dc6d9414c34b784db464f7e243b11c58d1
Chris <cmancrypto@outlook.com>
2024-01-14 14:39:54 +1100

    added new tokens to Custom basket added a CSV output to custom basket

--- Kleomedes_custom_basket.py
+++ Kleomedes_custom_basket.py
@@ -1,7 +1,7 @@
 from tools import get_coin_market_caps_for_date_list, df_from_list_dicts
 import datetime
 
-def get_Kleomedes_custom_basket_results(dates,coin_names):
+def get_Kleomedes_custom_basket_results(dates,coin_names,make_csv=False):
     # get the market caps for the dates and the coin list and print the results
     market_caps = get_coin_market_caps_for_date_list(dates, coin_names)
     #create the dataframe
@@ -11,19 +11,24 @@ def get_Kleomedes_custom_basket_results(dates,coin_names):
     for date in dates:
         market_cap = dataframes[f"{date}"].sum()
         print(f"Kleo custom basket market cap at {date} is {market_cap}")
+    if make_csv==True:
+        dataframes.to_csv("custom_basket_results.csv")
+    return dataframes
 
 
 if __name__ == '__main__':
     print("Kleomedes Custom Basket")
 
     # these are the chains in the current Kleomedes custom basket
-    coin_names = ["akash-network", "juno-network", "kujira", "fetch-ai", "evmos", "stargaze", "persistence"]
+    ##coin_names = ["akash-network", "juno-network", "kujira", "fetch-ai", "evmos", "stargaze", "persistence","sentinel","cudos"]
+    coin_names = ["akash-network","juno-network", "kujira", "fetch-ai", "evmos", "stargaze", "persistence", "sentinel",
+                  "cudos"]
 
 
     # dates in datetime.datetime(YYYY, MM, DD) format
-    dates = [datetime.datetime(2024, 1, 1), datetime.datetime(2023, 1, 1)]
+    dates = [datetime.datetime(2023, 12, 29), datetime.datetime(2023, 2, 3)]
 
     #call the function to run the script
-    get_Kleomedes_custom_basket_results(dates,coin_names)
+    get_Kleomedes_custom_basket_results(dates,coin_names,make_csv=True)
 
 
